606f4d515fa48f002b50a197ac6a3c47ca99a83c
Robert M Ochshorn <mail@RMOZONE.COM>
2012-06-25 16:03:36 +0200

    make linewidth explicit

--- calibration.py
+++ calibration.py
@@ -13,33 +13,47 @@ BOX_WIDTH = 600*2
 
 # lines & checkerboard
 
-def lines(linewidth, density=0.5):
-    lineperiod = linewidth / density
+def lines(width, density=0.5):
+    period = width / density
+
+    rem = period % 1.0
+    print 'ERROR: %.2f%%' % (100*rem/period)
+
+    period = int(period)
+
     out = numpy.zeros((BOX_WIDTH,BOX_WIDTH), dtype=numpy.bool)
     curx = 0
-    while curx<BOX_WIDTH-linewidth:
-        out[:,curx:curx+linewidth] = True
-        curx += lineperiod
+    while curx<BOX_WIDTH-width:
+        out[:,curx:curx+width] = True
+        curx += period
     return out
 
-def grid(linewidth, density=0.5):
-    lineperiod = linewidth * ((1 + numpy.sqrt(1-density))/density)
-    print lineperiod
+def grid(width, density=0.5):
+    period = width * ((1 + numpy.sqrt(1-density))/density)
+
+    rem = period % 1.0
+    print 'ERROR: %.2f%%' % (100*rem/period)
+
+    period = int(period)
+
     out = numpy.ones((BOX_WIDTH,BOX_WIDTH), dtype=numpy.bool)
     curx = 0
-    while curx<BOX_WIDTH-linewidth:
-        out[:,int(curx):int(curx)+linewidth] = False
-        curx += lineperiod
+    while curx<BOX_WIDTH-width:
+        out[:,curx:curx+width] = False
+        curx += period
     cury = 0
-    while cury<BOX_WIDTH-linewidth:
-        out[int(cury):int(cury)+linewidth] = False
-        cury += lineperiod
+    while cury<BOX_WIDTH-width:
+        out[cury:cury+width] = False
+        cury += period
     return out
 
 a4 = numpy.zeros((HEIGHT,WIDTH), numpy.bool)
 fns = [lines, grid]
 
-for l_idx,numlines in enumerate(numpy.arange(1,9)*10):
+linewidths = [5,10,15,20,30,40,50,60]
+
+for l_idx,linewidth in enumerate(linewidths):
+    print linewidth
 
     cury = (l_idx % 4)*(BOX_WIDTH+PADDING)
 
@@ -47,11 +61,11 @@ for l_idx,numlines in enumerate(numpy.arange(1,9)*10):
 
         curx = int(l_idx / 4 + 2*f_idx)*(BOX_WIDTH+PADDING)
 
-        linewidth = BOX_WIDTH/float(2*numlines)
-        print linewidth
+        # linewidth = BOX_WIDTH/float(2*numlines)
+        # print linewidth
 
         a4[int(cury):int(cury)+BOX_WIDTH,int(curx):int(curx)+BOX_WIDTH] = fn(linewidth)
 
 
 im = Image.fromarray((255*a4).astype(numpy.uint8), mode='L')
-im.save('calib.tif')
+im.save('calib2.tif')
